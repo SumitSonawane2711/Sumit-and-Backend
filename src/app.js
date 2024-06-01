51a92566fec7a61d39b68570da730dda5b7dbd8b
Sumit <sumitrs2711@gmail.com>
2024-06-01 23:15:30 +0530

    add comments

--- src/app.js
+++ src/app.js
@@ -4,15 +4,18 @@ import cookieParser from 'cookie-parser';
 
 const app = express()
 
-app.use(cors({
+app.use(cors({                        // handle the cors
     origin: process.env.CORS_ORIGIN,
     credentials :true
 }))
 
+
+// add configuration for recieved data from frontend in diff from
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public")) //add assets in public folder of local storage (ex.images,etc.)
+
+app.use(cookieParser()) // to access the cookies from the browser of user through server
 
 
 //routes import
